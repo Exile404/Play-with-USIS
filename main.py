4a7e3dfd53a9d20776402050e61e7ad9f4928229
Tahsinul Haque Dhrubo <71776452+thDhrubo@users.noreply.github.com>
2023-01-24 01:56:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Backend.backend import Usis
 
 with Usis() as bot:
     bot.land_first_page()
-    bot.login('nfsdhrubo@gmail.com','Dhrubo@CSE20')
+    bot.login('nfsdhrubo@gmail.com','^^^')
     bot.seat_status_option()
     bot.seat_stattus_selection('2022','Fall 2022','CSE220')
 
